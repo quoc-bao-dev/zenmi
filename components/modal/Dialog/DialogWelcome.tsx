3aef406722bc4acc476b47c4f8b7d5f2457d0989
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2024-06-27 15:19:53 +0700

    updated

--- components/modal/Dialog/DialogWelcome.tsx
+++ components/modal/Dialog/DialogWelcome.tsx
@@ -28,7 +28,7 @@ export function DialogWelcome() {
                     <DialogContent className="w-[95%] max-h-[95vh] border-0 !bg-transparent shadow-none focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0">
                         <DialogHeader className="relative">
                             <div className="">
-                                <Image src={'/example/shop/baby-pana.png'} width={1280} height={1024} alt="" className="object-cover" />
+                                <Image src={'/example/shop/baby-update.png'} width={1280} height={1024} alt="" className="object-cover" />
                             </div>
                             <div onClick={() => setOpenDialogWelcome(false)} className="absolute -bottom-10 left-1/2 -translate-x-1/2 bg-red-500 rounded-full hover:scale-105 cursor-pointer transition-all duration-150 ease-linear">
                                 <X size={32} fontWeight={600} className=" text-white p-2" />
